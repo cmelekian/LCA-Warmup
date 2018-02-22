66a04929fb80ebde14830ff68d72c0eb3ef39322
cmelekian <chris.melekian@gmail.com>
2018-02-22 17:08:06 -0500

    playing with imports and empty spaces

--- src/main/java/net/melekian/lca/FindRecentAncestors.java
+++ src/main/java/net/melekian/lca/FindRecentAncestors.java
@@ -1,13 +1,8 @@
 package net.melekian.lca;
-import org.jgrapht.Graph;
-import org.jgrapht.graph.SimpleDirectedGraph;
-import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.alg.NaiveLcaFinder;
-import org.jgrapht.io.DOTImporter;
-import org.jgrapht.io.ImportException;
-import org.jgrapht.io.VertexProvider;
-import org.jgrapht.io.EdgeProvider;
-import org.jgrapht.io.buildVertex;
+import org.jgrapht.*;
+import org.jgrapht.graph.*;
+import org.jgrapht.alg.*;
+import org.jgrapht.io.*;
 import java.util.*;
 import java.io.*;
 
@@ -19,6 +14,7 @@ public class FindRecentAncestors {
     	
     	VertexProvider<String> getName = new VertexProvider<String>() {
     		public String buildVertex(String id, Map<String, Attribute> attributes) {
+    			//Gets the correct name in case of regnal numbers or nicknames
     			if (attributes.containsKey("label")) {
     				return attributes.get("label").toString();
     			}
@@ -42,9 +38,11 @@ public class FindRecentAncestors {
     	}
     	catch (FileNotFoundException e){
     		System.out.println("Could not find the specified file.");
+    		return null;
     	}
     	catch (ImportException e) {
     		System.out.println("Could not import a graph from the specified file.");
+    		return null;
     	}
     	finally {
     		try {
@@ -52,35 +50,40 @@ public class FindRecentAncestors {
     		}
     		catch (IOException e) {
     			System.out.println("Something bad happened.");
+    			return null;
     		}
     	}
     	
-    	/*
-    	 * Eliminate marriages before returning
-    	 */
+    	//Eliminate marriages since they don't reflect an ancestral relation
     	for (String u : familyTree.vertexSet()) {
     		for (String v : familyTree.vertexSet()) {
     			if ((familyTree.containsEdge(u,v)) && (familyTree.containsEdge(v,u))) {
     				familyTree.removeEdge(u,v);
     				familyTree.removeEdge(v,u);
     			}
-    		}
-    		
+    		}	
+    	}
+    	
+    	//If the graph still contains cycles at this point we can't do anything to fix it
+    	CycleDetector<String, DefaultEdge> C = new CycleDetector<>(familyTree);
+    	if (C.detectCycles()) {
+    		System.out.println("The graph contains a cycle.");
+    		return null;
     	}
     	
     	return familyTree;
     }
     
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         String file = args[0];
         String person1 = args[1];
         String person2 = args[2];
         Graph<String, DefaultEdge> familyTree = getFamilyTree(file);
-        
-        NaiveLcaFinder<String, DefaultEdge> lca = new NaiveLcaFinder<>(familyTree);
-        for (String p : lca.findLcas(person1, person2)) {
-        	System.out.println(p);
+        if (familyTree != null) {
+        	NaiveLcaFinder<String, DefaultEdge> lca = new NaiveLcaFinder<>(familyTree);
+        	for (String p : lca.findLcas(person1, person2)) {
+        		System.out.println(p);
+        	}	
         }
     }
 }
